884241bf779ca7ad91088aec5ddd3cc63e8728a5
Alex Langdon <aalangdon4@gmail.com>
2020-10-16 23:53:54 +0100

    Updating item layout for collection preview

--- src/components/collection-preview/collection-preview.component.tsx
+++ src/components/collection-preview/collection-preview.component.tsx
@@ -6,7 +6,7 @@ import CollectionPreviewItem from "../collection-preview-item/collection-preview
 export default function CollectionPreview(props: Collection): JSX.Element {
 	return <Container className="collection-preview">
 		<h1 className="title">{props.title}</h1>
-		<div className="preview">
+		<div className="preview d-flex flex-wrap justify-content-md-between justify-content-around">
 			{
 				props.items.map(item => (
 					<CollectionPreviewItem key={item.id} {...item} />
