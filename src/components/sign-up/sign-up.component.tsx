47ed74ef9dc1eb0f82ea4c15a64b23f88382de2e
Alex Langdon <aalangdon4@gmail.com>
2020-10-17 12:20:53 +0100

    Adding initial signup page form

---
+++ src/components/sign-up/sign-up.component.tsx
@@ -0,0 +1,28 @@
+import { Button, FormGroup, TextField } from "@material-ui/core";
+import React from "react";
+
+export class SignUp extends React.Component {
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I don't have an account</h2>
+				<h5>Sign up with your email and password</h5>
+				<form>
+					<FormGroup>
+						<TextField label="Display Name" type="text" id="sign-up-display-name"
+							inputProps={{ "aria-label": "display-name" }} />
+						<TextField label="Email" type="email" id="sign-up-email"
+							inputProps={{ "aria-label": "email" }} />
+						<TextField label="Password" type="password" id="sign-up-password"
+							inputProps={{ "aria-label": "password" }} />
+						<TextField label="Confirm Password" type="password" id="sign-up-password-confirm"
+							inputProps={{ "aria-label": "password" }} />
+					</FormGroup>
+					<Button variant="contained" color="primary" className="mr-3 mt-3">
+						Sign Up
+					</Button>
+				</form>
+			</div>
+		)
+	}
+}
--- src/pages/sign-in-page/sign-in-sign-up-page.component.tsx
+++ src/pages/sign-in-page/sign-in-sign-up-page.component.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { SignIn } from "../../components/sign-in/sign-in.component";
+import { SignUp } from "../../components/sign-up/sign-up.component";
 
 export default function SignInAndSignUpPage() {
 	return <div className="row">
 		<div className="col-md-6 mt-4">
 			<SignIn />
+		</div>
+		<div className="col-md-6 mt-4">
+			<SignUp />
 		</div>
 	</div>
 }
