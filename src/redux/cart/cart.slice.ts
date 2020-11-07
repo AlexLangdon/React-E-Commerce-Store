4821831a2a71b5d71af77a1a263a1e56f2d06d08
Alex Langdon <aalangdon4@gmail.com>
2020-11-07 18:36:39 +0000

    Cart reducer now tracks the number of each item

--- src/redux/cart/cart.slice.ts
+++ src/redux/cart/cart.slice.ts
@@ -1,17 +1,43 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import CollectionItem from "../../models/CollectionItem";
 
-const initialState: Array<CollectionItem> = [];
+interface CartEntry {
+	item: CollectionItem;
+	quantity: number;
+}
+
+const initialState: Array<CartEntry> = [];
 
 const cartSlice = createSlice({
 	name: "cart",
 	initialState,
 	reducers: {
 		addItemToCart(state, action: PayloadAction<CollectionItem>) {
-			return [...state, action.payload];
+			const itemToAdd = action.payload;
+			const itemCartIndex = state.findIndex(entry => entry.item.id === itemToAdd.id);
+			const newState = [...state];
+
+			if (itemCartIndex > -1) {
+				const matchingEntry = newState[itemCartIndex];
+				newState[itemCartIndex] = {
+					...matchingEntry,
+					quantity: matchingEntry.quantity + 1
+				};
+			} else {
+				newState.push({
+					item: itemToAdd,
+					quantity: 1
+				})
+			}
+
+			return newState;
 		},
 		removeItemFromCart(state, action: PayloadAction<CollectionItem>) {
-			return state.filter(item => item.id !== action.payload.id);
+			return state.map(entry => 
+				entry.item.id === action.payload.id 
+				? { ...entry, quantity: entry.quantity - 1 } 
+				: entry
+			).filter(entry => entry.quantity > 0);
 		}
 	}
 })
