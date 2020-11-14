5a2471cca36b97ca5ae9fabe0d225650cffd0020
Alex Langdon <aalangdon4@gmail.com>
2020-11-14 20:46:27 +0000

    Making cart icon appear clickable with anon-hover border

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -5,10 +5,8 @@ import { RootState } from "../../redux/root-reducer";
 import "./cart-icon.component.scss";
 
 export default function CartIcon() {
-	const cartItemCount = useSelector(
-		(state: RootState) => state.cart.reduce(
-			(runningTotal, item) => runningTotal + item.quantity, 0
-		)
+	const cartItemCount = useSelector((state: RootState) => state.cart).reduce(
+		(runningTotal, item) => runningTotal + item.quantity, 0
 	)
 
 	return (
