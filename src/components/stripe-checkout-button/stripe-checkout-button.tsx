8440804e3a8b3903c7392c76b61b0b2b6301fd9e
Alex Langdon <aalangdon4@gmail.com>
2020-11-24 00:44:46 +0000

    Adding stripe integration

---
+++ src/components/stripe-checkout-button/stripe-checkout-button.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@material-ui/core";
+import PaymentIcon from "@material-ui/icons/Payment";
+import React from "react";
+import StripeCheckout, { Token } from "react-stripe-checkout";
+
+
+export default function StripeCheckoutButton(props: { price: number }) {
+	const priceForStripe = props.price * 100;
+	const publishableKey = "pk_test_51HoJ7TGUCEgQsZQhoUYOUim6d1OOuOC4G8RVTetyf3M6iCoQEPbsAWnpU1Tg8etWMDpKq7i288OPFxbVrQH6sHs000xyUI0I8b";
+
+	const onToken = (token: Token) => {
+		console.log(token);
+		alert("Payment Success");
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="Nova Apparel Ltd."
+			billingAddress
+			shippingAddress
+			description={`Your total is £${props.price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			locale="auto"
+			currency="GBP"
+			stripeKey={publishableKey}
+		>
+			<Button type="submit" variant="contained" color="primary"
+				aria-label="pay-now" size="medium" endIcon={<PaymentIcon />}>
+				Pay Now
+			</Button>
+		</StripeCheckout >
+	);
+}
--- src/pages/checkout-page/checkout-page.component.tsx
+++ src/pages/checkout-page/checkout-page.component.tsx
@@ -1,10 +1,12 @@
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import AlertTitle from "@material-ui/lab/AlertTitle";
 import React from "react";
 import { useSelector } from "react-redux";
 import CheckoutTableRow from "../../components/checkout-table-row/checkout-table-row.component";
+import StripeCheckoutButton from "../../components/stripe-checkout-button/stripe-checkout-button";
 import CartEntryProps from "../../models/CartEntryProps";
 import { cartEntriesSelector, cartTotalCostSelector } from "../../redux/cart/cart.slice";
-import "./checkout-page.component.scss";
 
 export default function CheckoutPage() {
 	const entries = useSelector(cartEntriesSelector);
@@ -32,6 +34,17 @@ export default function CheckoutPage() {
 						<TableRow>
 							<TableCell colSpan={4} />
 							<TableCell align="center"><b>TOTAL: £{totalCost}</b></TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell colSpan={4}>
+								<Alert severity="warning">
+									<AlertTitle>Please use the following credit card details for testing:</AlertTitle>
+									Card number: 4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+								</Alert>
+							</TableCell>
+							<TableCell align="center">
+								<StripeCheckoutButton price={totalCost} />
+							</TableCell>
 						</TableRow>
 					</TableBody>
 				</Table>
