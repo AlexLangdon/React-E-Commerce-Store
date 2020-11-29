0e714aeb3de6aaed6477e188826ce2e32b3c4c30
Alex Langdon <aalangdon4@gmail.com>
2020-11-29 11:50:37 +0000

    Restricting redux-logs to development environment

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, Middleware } from "redux";
 import logger from "redux-logger";
 import persistStore from "redux-persist/es/persistStore";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares: Array<Middleware> = [];
+
+if (process.env.NODE_ENV === "development") {
+	middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
