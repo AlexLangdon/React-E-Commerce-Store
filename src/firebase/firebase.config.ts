ad57c3b8b92a47f0b0c48b1658368ee806ef160f
Alex Langdon <aalangdon4@gmail.com>
2020-11-29 00:52:23 +0000

    Adding firebase config file for public access

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyBdz1p3-uorXsmP5V0fz2fDA8TF17r3q24",
+	authDomain: "e-commerce-store-8327b.firebaseapp.com",
+	databaseURL: "https://e-commerce-store-8327b.firebaseio.com",
+	projectId: "e-commerce-store-8327b",
+	storageBucket: "e-commerce-store-8327b.appspot.com",
+	messagingSenderId: "387986361116",
+	appId: "1:387986361116:web:99671082691e0d6fc1c8d9",
+	measurementId: "G-JFY9REZRPC"
+} as const;
